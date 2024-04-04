bebd938c11e48b66b1c12396ac7ed1dfea2524b3
Kristian <friis1212@gmail.com>
2024-04-04 09:21:15 +0200

    test

--- SimpleWorkoutPlanner.Client/Program.cs
+++ SimpleWorkoutPlanner.Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SimpleWorkoutPlanner.Client;
+using BootstrapMobile.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBootstrapMobile();
+builder.Services.AddScoped<BToastService>();
 
 await builder.Build().RunAsync();
